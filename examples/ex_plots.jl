2f9c869d549ba701dd47722df1fba37745c32b2b
Will Pazner <will.e.p@gmail.com>
2021-01-05 12:17:27 -0800

    Fix plots example

--- examples/ex_plots.jl
+++ examples/ex_plots.jl
@@ -3,7 +3,17 @@ using Images, Plots, Triplot
 
 include("common.jl")
 
+function to_rgba(x::UInt32)
+    a = ((x & 0xff000000)>>24)/255
+    b = ((x & 0x00ff0000)>>16)/255
+    g = ((x & 0x0000ff00)>>8)/255
+    r = (x & 0x000000ff)/255
+    RGBA(r, g, b, a)
+end
+
 x,y,t = cartmesh(100)
+z = peaks_unit.(x,y)
+
 GR.setcolormap(1)
-img = reinterpret(RGB24,Triplot.rasterize(x,y,peaks_unit.(x,y),t))
-plot([0,1],[0,1],img,yflip=false)
+img = to_rgba.(Triplot.rasterize(x,y,z,t)')
+plot([extrema(x)...],[extrema(y)...],img,yflip=false)
